66f2552a5810788353fd155c68f92b81383a3777
Dehan667 <wg.akame@gmail.com>
2021-12-13 16:37:42 +0100

    Javascript

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 // ouais c'est la java script cc
+
+const valeur1 = document.getElementById("valeur1");
+const valeur2 = document.getElementById("valeur2");
+const resultat = document.getElementById("resultat");
+
+console.log(valeur1);
+console.log(valeur2);
+console.log(resultat);
